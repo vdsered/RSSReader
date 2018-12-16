44ade4affa1c5ee14453d5d4f57da5a64b19a373
daniil <vdsered@gmail.com>
2018-12-16 23:09:58 +0700

    Initial commit

---
+++ app/src/main/java/com/companyname/rssreader/MixedFeedActivity.java
@@ -0,0 +1,12 @@
+package com.companyname.rssreader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public final class MixedFeedActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mixed_feed_layout);
+    }
+}
