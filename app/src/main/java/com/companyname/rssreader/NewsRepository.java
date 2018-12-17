4d05400864deeda3a8ef050e8ed6f1b2e06790c7
daniil <vdsered@gmail.com>
2018-12-18 03:57:21 +0700

    1) DataSources are not being created inside NewsRepository 2) There is a limit for description's length 3) HTML tags in description are always eliminated

--- app/src/main/java/com/companyname/rssreader/NewsRepository.java
+++ app/src/main/java/com/companyname/rssreader/NewsRepository.java
@@ -1,36 +1,58 @@
 package com.companyname.rssreader;
 
+import android.text.Html;
+
 import java.util.Comparator;
 
 import io.reactivex.Observable;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.schedulers.Schedulers;
 
 public final class NewsRepository {
-
-    private static class TimeComparator implements Comparator<RSS2Item> {
-        @Override
-        public int compare(RSS2Item o1, RSS2Item o2) {
-            return 0;
-        }
+    public NewsRepository(@NonNull FeedDataSource habr, @NonNull FeedDataSource meduza) {
+        this.habr = habr;
+        this.meduza = meduza;
     }
 
     public Observable<News> retrieveNewsSince(long timestamp, int count)
     {
-        final Observable<RSS2Item> habrObservable = habr.retrieveItemsSince(timestamp, count);
-        final Observable<RSS2Item> meduzaObservable = habr.retrieveItemsSince(timestamp, count);
+        final Observable<FeedEntry> habrObservable =
+                habr.retrieveEntriesSince(timestamp, count);
+        final Observable<FeedEntry> meduzaObservable =
+                meduza.retrieveEntriesSince(timestamp, count);
 
         return Observable
                 .merge(habrObservable, meduzaObservable)
                 .toSortedList(new TimeComparator())
                 .flatMapObservable(Observable::fromIterable)
                 .take(count)
+                .observeOn(Schedulers.computation())
                 .map(item ->
                 {
-                    return new News(item.title, item.description, "", "", 0);
+                    final int MAX_CHARACTER_COUNT = 128;
+                    String description = eliminateHTMLTags(item.description);
+                    final int endIndex = Math.min(description.length(), MAX_CHARACTER_COUNT);
+                    return new News(item.title, description.substring(0, endIndex),
+                            "", "", item.timestamp);
                 });
     }
 
-    private final RSS2DataSource habr =
-            new RSS2DataSource("https://habr.com/rss/hubs/all/");
-    private final RSS2DataSource meduza =
-            new RSS2DataSource("https://meduza.io/rss/podcasts/meduza-v-kurse");
+    private String eliminateHTMLTags(String text)
+    {
+        /*
+            TODO
+            Should it remove HTML tags via regex?
+         */
+        return Html.fromHtml(text).toString();
+    }
+
+    private static class TimeComparator implements Comparator<FeedEntry> {
+        @Override
+        public int compare(FeedEntry left, FeedEntry right) {
+            return Long.signum(right.timestamp - left.timestamp);
+        }
+    }
+
+    private final FeedDataSource habr;
+    private final FeedDataSource meduza;
 }
