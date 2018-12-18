de64c8e6e1cb9f78bef205fe27171d4050d9c795
daniil <vdsered@gmail.com>
2018-12-18 09:50:56 +0700

    Added a method FeedView to set default images for news

--- app/src/main/java/com/companyname/rssreader/FeedView.java
+++ app/src/main/java/com/companyname/rssreader/FeedView.java
@@ -1,8 +1,11 @@
 package com.companyname.rssreader;
 
+import java.util.List;
+
 public interface FeedView {
     void showNews(News news);
     void showErrorMessage();
+    void setDefaultNewsImages(List<Logo> logos);
     void showLoading();
     void hideLoading();
 }
