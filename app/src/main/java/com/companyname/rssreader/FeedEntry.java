879117e89cd76fa856a13a0ff741cffae37e2041
daniil <vdsered@gmail.com>
2018-12-18 09:50:15 +0700

    Added sourceHost field to FeedEntry

--- app/src/main/java/com/companyname/rssreader/FeedEntry.java
+++ app/src/main/java/com/companyname/rssreader/FeedEntry.java
@@ -4,10 +4,11 @@ public final class FeedEntry {
     public final String description;
     public final String title;
     public final long timestamp;
-
-    public FeedEntry(String description, String title, long timestamp) {
+    public final String sourceHost;
+    public FeedEntry(String description, String title, long timestamp, String sourceHost) {
         this.description = description;
         this.title = title;
         this.timestamp = timestamp;
+        this.sourceHost = sourceHost;
     }
 }
