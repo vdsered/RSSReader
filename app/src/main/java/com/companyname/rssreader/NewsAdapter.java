cd8bfc47e8a5e2ba7f234f5353c95294a2f1330d
daniil <vdsered@gmail.com>
2018-12-18 09:52:59 +0700

    1) NewsAdapter uses a logo if there is no image for a news 2) Primitive expansion state change of news (simple->extended, extended->simple)

--- app/src/main/java/com/companyname/rssreader/NewsAdapter.java
+++ app/src/main/java/com/companyname/rssreader/NewsAdapter.java
@@ -1,15 +1,24 @@
 package com.companyname.rssreader;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public final class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsViewHolder> {
 
@@ -26,12 +35,59 @@ public final class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsView
         return new NewsViewHolder(feedItemView);
     }
 
+    public void setLogos(List<Logo> logos) {
+        for(final Logo logo : logos) {
+            sourceLogos.put(logo.host, logo.logoURL);
+        }
+        int position = 0;
+        for(final News news : items) {
+
+            if(news.imageURL.isEmpty()) {
+                notifyItemChanged(position);
+            }
+            ++position;
+        }
+    }
+
     @Override
-    public void onBindViewHolder(@NonNull NewsViewHolder newsViewHolder, int position) {
+    public void onBindViewHolder(@NonNull NewsViewHolder newsViewHolder, final int position) {
         final News news = items.get(position);
 
         newsViewHolder.title.setText(news.title);
         newsViewHolder.description.setText(news.description);
+
+        newsViewHolder.description.setVisibility(View.GONE);
+
+        newsViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final int adapterPosition = newsViewHolder.getAdapterPosition();
+                expansionStates.set(adapterPosition, !expansionStates.get(adapterPosition));
+                notifyItemChanged(adapterPosition);
+            }
+        });
+
+        final boolean expansionState = expansionStates.get(position);
+
+        newsViewHolder
+                .description
+                .setVisibility(expansionState ? View.VISIBLE : View.GONE);
+
+        final Drawable drawable = null;
+        newsViewHolder.image.setImageDrawable(drawable);
+
+        final String imageUrl;
+        if(news.imageURL.isEmpty())
+           imageUrl = sourceLogos.get(news.source);
+        else
+            imageUrl = news.imageURL;
+        Glide.with(newsViewHolder.image.getContext())
+                .load(imageUrl)
+                .into(newsViewHolder.image);
+
+        newsViewHolder.source.setText(news.source);
+        final String date = DateFormat.format("dd-MM-yyyy hh:mm:ss", news.timestamp).toString();
+        newsViewHolder.date.setText(date);
     }
 
     public void addNews(List<News> news)
@@ -39,6 +95,7 @@ public final class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsView
         final int startPosition = items.size();
         final int count = news.size();
         items.addAll(startPosition, news);
+        expansionStates.addAll(startPosition, Collections.nCopies(count, false));
         notifyItemRangeInserted(startPosition, count);
     }
 
@@ -51,17 +108,24 @@ public final class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsView
     public int getItemCount() {
         return items.size();
     }
-
     static class NewsViewHolder extends RecyclerView.ViewHolder {
+        public final TextView source;
         public final TextView title;
         public final TextView description;
+        public final TextView date;
+        public final ImageView image;
         public NewsViewHolder(@NonNull View itemView) {
             super(itemView);
+            image = itemView.findViewById(R.id.image);
             title = itemView.findViewById(R.id.title);
+            source = itemView.findViewById(R.id.source);
+            date = itemView.findViewById(R.id.date);
             description = itemView.findViewById(R.id.description);
         }
     }
 
+    private Map<String, String> sourceLogos = new HashMap<>();
     private LayoutInflater inflater;
     private final List<News> items = new ArrayList<>();
+    private final List<Boolean> expansionStates = new ArrayList<>();
 }
