0def2d136e6fa74ff985d0d59fcf1f903d109ccd
daniil <vdsered@gmail.com>
2018-12-18 09:25:32 +0700

    Added logo loading to NewsRepository

--- app/src/main/java/com/companyname/rssreader/NewsRepository.java
+++ app/src/main/java/com/companyname/rssreader/NewsRepository.java
@@ -3,8 +3,12 @@ package com.companyname.rssreader;
 import android.text.Html;
 
 import java.util.Comparator;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import io.reactivex.Observable;
+import io.reactivex.Single;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.schedulers.Schedulers;
 
@@ -12,6 +16,13 @@ public final class NewsRepository {
     public NewsRepository(@NonNull FeedDataSource habr, @NonNull FeedDataSource meduza) {
         this.habr = habr;
         this.meduza = meduza;
+    }
+
+    public Single<List<Logo>> retrieveLogos() {
+        final Single<Logo> habrSingle = habr.getLogo();
+        final Single<Logo> meduzaSingle = meduza.getLogo();
+        return Observable.merge(habrSingle.toObservable(), meduzaSingle.toObservable())
+                         .toList().subscribeOn(Schedulers.io());
     }
 
     public Observable<News> retrieveNewsSince(long timestamp, int count)
@@ -30,15 +41,25 @@ public final class NewsRepository {
                 .map(item ->
                 {
                     final int MAX_CHARACTER_COUNT = 128;
-                    String description = eliminateHTMLTags(item.description);
+                    final String imageURL = extractImageURL(item.description);
+                    final String description = eliminateHTMLTags(item.description);
                     final int endIndex = Math.min(description.length(), MAX_CHARACTER_COUNT);
                     return new News(item.title, description.substring(0, endIndex),
-                            "", "", item.timestamp);
+                                    imageURL, item.sourceHost, item.timestamp);
                 });
     }
 
-    private String eliminateHTMLTags(String text)
-    {
+    private String extractImageURL(String description) {
+        final Pattern pattern = Pattern.compile("src=\"(.*?)\"");
+        final Matcher matcher = pattern.matcher(description);
+        if (matcher.find()) {
+            final int capturingGroup = 1;
+            return matcher.group(capturingGroup);
+        }
+        return "";
+    }
+
+    private String eliminateHTMLTags(String text) {
         /*
             TODO
             Should it remove HTML tags via regex?
